f5937615accb50f6c8b360c83e18d142186354a8
Aurélien <lefty.wolf.idk@hotmail.com>
2022-12-05 16:45:44 +0100

    Ajout du footer
    Ajout du footer, et intégration du footer dans main.css

--- e-commerce/includes/footer.php
+++ e-commerce/includes/footer.php
@@ -1,1 +1,32 @@
-<?php echo 'footer'; ?>
+<div class="footer bg-dark">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-sm-4 col-md-3 item">
+				<ul>
+					<li>Notre Histoire</li>
+					<li>Nous Contacter</li>
+				</ul>
+			</div>
+			<div class="col-sm-4 col-md-3 item">
+				<ul>
+					<li>Instagram</li>
+					<li>Facebook</li>
+					<li>mail</li>
+				</ul>
+			</div>
+			<div class="col-sm-4 col-md-3 item">
+				<ul>
+					<li>adresse, code postal, pays</li>
+					<li>+33 0 00 00 00 00</li>
+				</ul>
+			</div>
+			<div class="col-sm-4 col-md-3 item">
+				<ul>
+					<li>Condition d'utilisation</li>
+					<li>Gérer les données</li>
+					<li>Cookies</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
