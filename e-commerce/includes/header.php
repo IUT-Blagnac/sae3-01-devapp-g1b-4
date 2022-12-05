a59f42038ab9f19e923fe5cc1210d5bf9cd604ee
IUT BLAGNAC <noreply@univ-tlse2.fr>
2022-12-05 12:23:24 +0100

    Début du header sans php
    Début du header sans images, sans liens

--- e-commerce/includes/header.php
+++ e-commerce/includes/header.php
@@ -1,1 +1,11 @@
 <?php echo 'header'; ?>
+<div class='header'>
+	<a href="#"><img src='images/logo.png'></a>
+	<a href="#"> Blue Gym</a>
+	<input type="text" name="rechercheUser" placeholder="Rechercher">
+	<a href="#"><img src='images/cart.png'></a>
+	<a href="#"><img src="images/profil.png"></a>
+	<a href="#"><img src="images/lune.png"></a>
+	<a href="#"><img src="images/lang.png"></a>
+
+</div>
