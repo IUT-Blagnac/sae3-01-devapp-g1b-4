80d71c883e50836f93f5d1adf5aa398a578fe8e5
Gabin Munoz <32036115+Munozmu@users.noreply.github.com>
2022-12-05 12:07:01 +0100

    header/footer setting

--- e-commerce/index.php
+++ e-commerce/index.php
@@ -1,1 +1,25 @@
-<?php echo 'test' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BlueGym - dev</title>
+
+    <link rel="stylesheet" href="includes/bootstrap/css/bootstrap.min.css">
+
+</head>
+<body>
+
+    <?php include('includes/header.php'); ?>
+
+
+    <?php echo 'ça fonctionne'; ?>
+
+
+    <?php include('includes/footer.php'); ?>
+
+
+    
+</body>
+</html>
