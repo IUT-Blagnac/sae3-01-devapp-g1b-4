da3042f23f023600285cef07f7d99c65521c6486
Andrew <95313598+andrew-phakeovilay@users.noreply.github.com>
2022-12-06 16:18:04 +0100

    Ajout fichier de configuration et MAJ
    Modèle d'un fichier de configuration et mise à jour du fichier python
    Changements :
    - Code exécutable
    - Ajout de toutes vérifications de seuils
    - Ajout de la fréquence

--- iot/python/Python_SAE_IoT_1G4.py
+++ iot/python/Python_SAE_IoT_1G4.py
@@ -3,10 +3,6 @@ import json
 import yaml
 import paho.mqtt.client as mqtt
 
-tabCo2 = []
-tabTemp = []
-tabHum = []
-
 with open("config_mqtt.yaml", "r") as stream:
     try:
         config = yaml.safe_load(stream)
@@ -14,63 +10,75 @@ with open("config_mqtt.yaml", "r") as stream:
         print(exc)
 
 
-def get_data(obj, msg):
+def get_data(mqtt, obj, msg):
     jsonMsg = json.loads(msg.payload)
-    date = datetime.datetime.now().strftime("%Y-%b-%d %H-%M-%S")
-    message = date + "\n"
-    if config["object"]["activity"]:
-        message += "Activité : " + str(jsonMsg["object"]["activity"]) + " PIR\n"
-    if config["object"]["co2"]:
-        if jsonMsg["object"]["co2"] > 1400:
-            message += "[Attention, aérez si possible !] "
-        message += "Concentration CO2 : " + str(jsonMsg["object"]["co2"]) + " ppm\n"
-    if config["object"]["humidity"]:
-        if jsonMsg["object"]["humidity"] > 0:
-            message += "[Attention,] "
-        message += "Humidité : " + str(jsonMsg["object"]["humidity"]) + " %\n"
-    if config["object"]["illumination"]:
-        if jsonMsg["object"]["illumination"] > 0:
-            message += "[Attention,] "
-        message += "Éclairement : " + str(jsonMsg["object"]["illumination"]) + " lux\n"
-    if config["object"]["infrared"]:
-        if jsonMsg["object"]["infrared"] > 0:
-            message += "[Attention,] "
-        message += "Infrarouge : " + str(jsonMsg["object"]["infrared"]) + " µm\n"
-    if config["object"]["infrared_and_visible"]:
-        if jsonMsg["object"]["infrared_and_visible"] > 0:
-            message += "[Attention,] "
-        message += "Infrarouge visible : " + str(jsonMsg["object"]["infrared_and_visible"]) + " µm\n"
-    if config["object"]["pressure"]:
-        if jsonMsg["object"]["pressure"] > 1000.0:
-            message += "[Attention, risque de problèmes !] "
-        message += "Pression : " + str(jsonMsg["object"]["pressure"]) + " hPa\n"
-    if config["object"]["temperature"]:
-        if jsonMsg["object"]["temperature"] > 70 or jsonMsg["object"]["temperature"] < -20:
-            message += "[Attention, ] "
-        message += "Température : " + str(jsonMsg["object"]["temperature"]) + " °C\n"
-    if config["object"]["tvoc"]:
-        if jsonMsg["object"]["tvoc"] > 60000:
-            message += "[Attention, risque mortel !] "
-        message += "Qualité de l'air : " + str(jsonMsg["object"]["tvoc"]) + " ppb\n"
-    # moy_tableaux(donneeCo2, donneeTemp, donneeHum)
-    with open(date+" donnees.txt", "w", encoding="utf-8") as a_file:
-        a_file.write(message)
-        a_file.close()
+    message = ""
+    for i in range(config["frequency"]):
+
+        date = datetime.datetime.now().strftime("%Y-%b-%d %H-%M-%S")
+        message += date + "\n"
+        if config["object"]["activity"]:
+            if config["seuils"]["activity"] is not None:
+                if config["seuils"]["activity"] < jsonMsg["object"]["activity"]:
+                    message += "[Seuil dépassé!] "
+            message += "Activité : " + str(jsonMsg["object"]["activity"]) + " PIR\n"
+
+        if config["object"]["co2"]:
+            if config["seuils"]["co2"] is not None:
+                if config["seuils"]["co2"] < jsonMsg["object"]["co2"]:
+                    message += "[Seuil dépassé!] "
+            message += "Concentration CO2 : " + str(jsonMsg["object"]["co2"]) + " ppm\n"
+
+        if config["object"]["humidity"]:
+            if config["seuils"]["humidity"] is not None:
+                if config["seuils"]["humidity"] < jsonMsg["object"]["humidity"]:
+                    message += "[Seuil dépassé!] "
+            message += "Humidité : " + str(jsonMsg["object"]["humidity"]) + " %\n"
+
+        if config["object"]["illumination"]:
+            if config["seuils"]["illumination"] is not None:
+                if config["seuils"]["illumination"] < jsonMsg["object"]["illumination"]:
+                    message += "[Seuil dépassé!] "
+            message += "Éclairement : " + str(jsonMsg["object"]["illumination"]) + " lux\n"
+
+        if config["object"]["infrared"]:
+            if config["seuils"]["infrared"] is not None:
+                if config["seuils"]["infrared"] < jsonMsg["object"]["infrared"]:
+                    message += "[Seuil dépassé!] "
+            message += "Infrarouge : " + str(jsonMsg["object"]["infrared"]) + " µm\n"
+
+        if config["object"]["infrared_and_visible"]:
+            if config["seuils"]["infrared_and_visible"] is not None:
+                if config["seuils"]["infrared_and_visible"] < jsonMsg["object"]["infrared_and_visible"]:
+                    message += "[Seuil dépassé!] "
+            message += "Infrarouge visible : " + str(jsonMsg["object"]["infrared_and_visible"]) + " µm\n"
+
+        if config["object"]["pressure"]:
+            if config["seuils"]["pressure"] is not None:
+                if config["seuils"]["pressure"] < jsonMsg["object"]["pressure"]:
+                    message += "[Seuil dépassé!] "
+            message += "Pression : " + str(jsonMsg["object"]["pressure"]) + " hPa\n"
+
+        if config["object"]["temperature"]:
+            if config["seuils"]["temperature"] is not None:
+                if config["seuils"]["temperature"] < jsonMsg["object"]["temperature"]:
+                    message += "[Seuil dépassé!] "
+            message += "Température : " + str(jsonMsg["object"]["temperature"]) + " °C\n"
+
+        if config["object"]["tvoc"]:
+            if config["seuils"]["tvoc"] is not None:
+                if config["seuils"]["tvoc"] < jsonMsg["object"]["tvoc"]:
+                    message += "[Seuil dépassé!] "
+            message += "Qualité de l'air : " + str(jsonMsg["object"]["tvoc"]) + " ppb\n"
+
+        if i < 1:
+            message += "\n"
+
+    with open(date + " donnees.txt", "w", encoding="utf-8") as file:
+        file.write(message)
+        file.close()
     client.disconnect()
 
-"""
-def moy_tableaux(dCo2, dTemp, dHum):
-    global tabCo2, tabTemp, tabHum 
-    tabCo2.append(dCo2)
-    tabTemp.append(dTemp)
-    tabHum.append(dHum)
-    if len(tabCo2) >= 10:
-        print(sum(tabCo2[len(tabCo2) - 10 :] ) / 10)
-    if len(tabTemp) >= 10:
-        print(sum(tabTemp[len(tabTemp) - 10 :] ) / 10)
-    if len(tabHum) >= 10:
-        print(sum(tabHum[len(tabHum) - 10 :] ) / 10)
-"""
 
 print("Connexion aux locaux de Blue Gym...")
 
