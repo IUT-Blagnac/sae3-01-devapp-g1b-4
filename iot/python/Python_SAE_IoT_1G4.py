7bb98247d881aad929b5da00dd7200ff42c4cb15
Andrew <95313598+andrew-phakeovilay@users.noreply.github.com>
2022-12-07 15:05:45 +0100

    fichier de configuration et python
    MAJ du fichier de configuration :
    - Ajout d'intervalle pour certains seuils
    MAJ code python :
    - Ajout de fréquence
    - Le code crée désormais plusieurs fichiers au lieu d'un fichier

--- iot/python/Python_SAE_IoT_1G4.py
+++ iot/python/Python_SAE_IoT_1G4.py
@@ -1,7 +1,9 @@
 import datetime
 import json
+from time import sleep
 import yaml
 import paho.mqtt.client as mqtt
+import os
 
 with open("config_mqtt.yaml", "r") as stream:
     try:
@@ -9,75 +11,125 @@ with open("config_mqtt.yaml", "r") as stream:
     except yaml.YAMLError as exc:
         print(exc)
 
+try:
+    os.remove(config["name"] + "_activity_donnees.txt")
+except OSError:
+    pass
+try:
+    os.remove(config["name"] + "_co2_donnees.txt")
+except OSError:
+    pass
+try:
+    os.remove(config["name"] + "_humidity_donnees.txt")
+except OSError:
+    pass
+try:
+    os.remove(config["name"] + "_illumination_donnees.txt")
+except OSError:
+    pass
+try:
+    os.remove(config["name"] + "_infrared_donnees.txt")
+except OSError:
+    pass
+try:
+    os.remove(config["name"] + "_infrared_and_visible_donnees.txt")
+except OSError:
+    pass
+try:
+    os.remove(config["name"] + "_pressure_donnees.txt")
+except OSError:
+    pass
+try:
+    os.remove(config["name"] + "_temperature_donnees.txt")
+except OSError:
+    pass
+try:
+    os.remove(config["name"] + "_tvoc_donnees.txt")
+except OSError:
+    pass
+
 
 def get_data(mqtt, obj, msg):
     jsonMsg = json.loads(msg.payload)
-    message = ""
-    for i in range(config["frequency"]):
+    now = datetime.datetime.now()
+    date = now.strftime("%Y-%b-%d %H-%M-%S")
+    f = open("log.txt", "a")
+    f.write(date + "\n")
+    f.close()
+    if config["object"]["activity"]:
+        if config["seuils"]["activity"] is not None and config["seuils"]["activity"] < jsonMsg["object"]["activity"]:
+            if now.hour > 18 or now.hour < 6:
+                print("[Activité détecté en dehors des horaires!]")
+        with open(config["name"] + "_activity_donnees.txt", "a", encoding="utf-8") as file:
+            file.write(str(jsonMsg["object"]["activity"]) + " PIR\n")
+            file.close()
 
-        date = datetime.datetime.now().strftime("%Y-%b-%d %H-%M-%S")
-        message += date + "\n"
-        if config["object"]["activity"]:
-            if config["seuils"]["activity"] is not None:
-                if config["seuils"]["activity"] < jsonMsg["object"]["activity"]:
-                    message += "[Seuil dépassé!] "
-            message += "Activité : " + str(jsonMsg["object"]["activity"]) + " PIR\n"
+    if config["object"]["co2"]:
+        if config["seuils"]["co2"][0] is not None and config["seuils"]["co2"][1] is not None and (
+                config["seuils"]["co2"][1] < jsonMsg["object"]["co2"] or config["seuils"]["co2"][0] > jsonMsg["object"]["co2"]):
+            print("[Seuil Concentration de CO2 dépassé]")
+        with open(config["name"] + "_co2_donnees.txt", "a", encoding="utf-8") as file:
+            file.write(str(jsonMsg["object"]["co2"]) + " ppm\n")
+            file.close()
 
-        if config["object"]["co2"]:
-            if config["seuils"]["co2"] is not None:
-                if config["seuils"]["co2"] < jsonMsg["object"]["co2"]:
-                    message += "[Seuil dépassé!] "
-            message += "Concentration CO2 : " + str(jsonMsg["object"]["co2"]) + " ppm\n"
+    if config["object"]["humidity"]:
+        if config["seuils"]["humidity"][0] is not None and config["seuils"]["humidity"][1] is not None and (
+                config["seuils"]["humidity"][1] < jsonMsg["object"]["humidity"] or config["seuils"]["humidity"][0] >
+                jsonMsg["object"]["humidity"]):
+            print("[Seuil d'humidité dépassé]")
+        with open(config["name"] + "_humidity_donnees.txt", "a", encoding="utf-8") as file:
+            file.write(str(jsonMsg["object"]["humidity"]) + " %\n")
+            file.close()
 
-        if config["object"]["humidity"]:
-            if config["seuils"]["humidity"] is not None:
-                if config["seuils"]["humidity"] < jsonMsg["object"]["humidity"]:
-                    message += "[Seuil dépassé!] "
-            message += "Humidité : " + str(jsonMsg["object"]["humidity"]) + " %\n"
+    if config["object"]["illumination"]:
+        if config["seuils"]["illumination"] is not None and config["seuils"]["illumination"] < jsonMsg["object"]["illumination"]:
+            print("[Seuil d'intensité lumineuse dépassé!]")
+        with open(config["name"] + "_illumination_donnees.txt", "a", encoding="utf-8") as file:
+            file.write(str(jsonMsg["object"]["illumination"]) + " lux\n")
+            file.close()
 
-        if config["object"]["illumination"]:
-            if config["seuils"]["illumination"] is not None:
-                if config["seuils"]["illumination"] < jsonMsg["object"]["illumination"]:
-                    message += "[Seuil dépassé!] "
-            message += "Éclairement : " + str(jsonMsg["object"]["illumination"]) + " lux\n"
+    if config["object"]["infrared"]:
+        if config["seuils"]["infrared"] is not None and config["seuils"]["infrared"] < jsonMsg["object"]["infrared"]:
+            print("[Seuil d'infrarouge dépassé!]")
+        with open(config["name"] + "_infrared_donnees.txt", "a", encoding="utf-8") as file:
+            file.write(str(jsonMsg["object"]["infrared"]) + " µm\n")
+            file.close()
 
-        if config["object"]["infrared"]:
-            if config["seuils"]["infrared"] is not None:
-                if config["seuils"]["infrared"] < jsonMsg["object"]["infrared"]:
-                    message += "[Seuil dépassé!] "
-            message += "Infrarouge : " + str(jsonMsg["object"]["infrared"]) + " µm\n"
+    if config["object"]["infrared_and_visible"]:
+        if config["seuils"]["infrared_and_visible"] is not None and config["seuils"]["infrared_and_visible"] < jsonMsg["object"]["infrared_and_visible"]:
+            print("[Seuil d'infrarouge visible dépassé!]")
+        with open(config["name"] + "_infrared_and_visible_donnees.txt", "a", encoding="utf-8") as file:
+            file.write(str(jsonMsg["object"]["infrared_and_visible"]) + " µm\n")
+            file.close()
 
-        if config["object"]["infrared_and_visible"]:
-            if config["seuils"]["infrared_and_visible"] is not None:
-                if config["seuils"]["infrared_and_visible"] < jsonMsg["object"]["infrared_and_visible"]:
-                    message += "[Seuil dépassé!] "
-            message += "Infrarouge visible : " + str(jsonMsg["object"]["infrared_and_visible"]) + " µm\n"
+    if config["object"]["pressure"]:
+        if config["seuils"]["pressure"] is not None and config["seuils"]["pressure"] < jsonMsg["object"]["pressure"]:
+            print("[Seuil de pression dépassé!]")
+        with open(config["name"] + "_pressure_donnees.txt", "a", encoding="utf-8") as file:
+            file.write(str(jsonMsg["object"]["pressure"]) + " hPa\n")
+            file.close()
 
-        if config["object"]["pressure"]:
-            if config["seuils"]["pressure"] is not None:
-                if config["seuils"]["pressure"] < jsonMsg["object"]["pressure"]:
-                    message += "[Seuil dépassé!] "
-            message += "Pression : " + str(jsonMsg["object"]["pressure"]) + " hPa\n"
+    if config["object"]["temperature"]:
+        if config["seuils"]["temperature"][1] is not None and config["seuils"]["temperature"][0] is not None and (
+                config["seuils"]["temperature"][1] < jsonMsg["object"]["temperature"] or
+                config["seuils"]["temperature"][0] > jsonMsg["object"]["temperature"]):
+            print("[Seuil de temperature dépassé pour les produits de Blue Gym!]")
+        with open(config["name"] + "_temperature_donnees.txt", "a", encoding="utf-8") as file:
+            file.write(str(jsonMsg["object"]["temperature"]) + " °C\n")
+            file.close()
 
-        if config["object"]["temperature"]:
-            if config["seuils"]["temperature"] is not None:
-                if config["seuils"]["temperature"] < jsonMsg["object"]["temperature"]:
-                    message += "[Seuil dépassé!] "
-            message += "Température : " + str(jsonMsg["object"]["temperature"]) + " °C\n"
+    if config["object"]["tvoc"]:
+        if config["seuils"]["tvoc"] is not None and config["seuils"]["tvoc"] < jsonMsg["object"]["tvoc"]:
+            print("[Seuil de qualité d'air dépassé!]")
+        with open(config["name"] + "_tvoc_donnees.txt", "a", encoding="utf-8") as file:
+            file.write(str(jsonMsg["object"]["tvoc"]) + " ppb\n")
+            file.close()
 
-        if config["object"]["tvoc"]:
-            if config["seuils"]["tvoc"] is not None:
-                if config["seuils"]["tvoc"] < jsonMsg["object"]["tvoc"]:
-                    message += "[Seuil dépassé!] "
-            message += "Qualité de l'air : " + str(jsonMsg["object"]["tvoc"]) + " ppb\n"
-
-        if i < 1:
-            message += "\n"
-
-    with open(date + " donnees.txt", "w", encoding="utf-8") as file:
-        file.write(message)
-        file.close()
-    client.disconnect()
+    lines_in_file = open("log.txt", 'r').readlines()
+    if len(lines_in_file) == config["frequency"]:
+        os.remove("log.txt")
+        client.disconnect()
+    sleep(10)
 
 
 print("Connexion aux locaux de Blue Gym...")
