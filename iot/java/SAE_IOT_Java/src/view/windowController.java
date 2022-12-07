9a6faaf6a7e2e75a01278da9ddfc62c0015c5fb2
Aurélien <lefty.wolf.idk@hotmail.com>
2022-12-07 15:56:14 +0100

    Création squelette fenètre
    Création du squelette de la fenêtre, en java FX
    Le commit contient :
    - le lancement de l'application/fenêtre (appli.java)
    - le fxml de la fenêtre (window.fxml)
    - le controller de la fenêtre (windowController.java)

---
+++ iot/java/SAE_IOT_Java/src/view/windowController.java
@@ -0,0 +1,23 @@
+package view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+public class windowController implements Initializable {
+
+	@FXML
+	private ListView<String> listViewData;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
