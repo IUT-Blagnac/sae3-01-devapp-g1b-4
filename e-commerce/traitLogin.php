3721bd0203dcd2a4fdc960344110b1716655c31a
Gabin Munoz <32036115+Munozmu@users.noreply.github.com>
2022-12-12 20:13:45 +0100

    ras

--- e-commerce/traitLogin.php
+++ e-commerce/traitLogin.php
@@ -13,20 +13,21 @@ else {
     $verifMail =  oci_parse($connect, $reqVerif);
 
     $resultVerif = oci_execute($verifMail);
+
     if (!$resultVerif) {
     	$e = oci_error($resultVerif);  // on récupère l'exception liée au pb d'execution de la requete (violation PK par exemple)
 		print htmlentities($e['message'].' pour cette requete : '.$e['sqltext']);	
     } else {
-    	$statementBD = oci_fetch_assoc($resultVerif));
+    	$statementBD = oci_fetch_assoc($resultVerif);
     	if(empty($statementBD){
     		header('location:formLogin.php?msgErreur=Compte inexistant');
     	}
     	if (password_verify($mdp_client_a_verifier, $statementBD['motpasseclient'])) {
     		session_start();
     		$_SESSION['idClientIdentifie'] = $statementBD['idclient'];
-    		header('location:index.php'):
+    		header('location:index.php');
     	}
-    }    
+    } 
 }
 
 ?>
