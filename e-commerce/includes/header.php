c873d88c00d8777803215147be506c7744033b54
Gabin Munoz <32036115+Munozmu@users.noreply.github.com>
2022-12-12 13:05:08 +0100

    modals

--- e-commerce/includes/header.php
+++ e-commerce/includes/header.php
@@ -42,8 +42,8 @@ session_start();
 								</svg>
 							</button>
 							<ul class="dropdown-menu">
-								<li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">Connexion</a></li>
-								<li><a class="dropdown-item text-primary fw-bold" href="#">Inscription</a></li>
+								<li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#login-modal">Connexion</a></li>
+								<li><a class="dropdown-item text-primary fw-bold" href="#" data-bs-toggle="modal" data-bs-target="#register-modal"> Inscription</a></li>
 							</ul>
 						</div>
 					</div>
@@ -57,22 +57,112 @@ session_start();
 <?php include('includes/catHeader.php'); ?>
 
 <!-- Modals de connexion/inscription -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
+<div class="modal" id="login-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="modal-title fs-5" id="exampleModalLabel">Connexion</h1>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<div class="modal-body">
+
+				<p class="text-muted">Si vous possédez déjà un compte sur notre site vous pouvez vous connecter avec vos identifiants. </p>
+
+				<form action="">
+					<div class="mb-3">
+						<label for="exampleInputEmail1" class="form-label">Adresse mail</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+					</div>
+					<div class="mb-3">
+						<label for="exampleInputPassword1" class="form-label"> Mot de passe</label>
+						<input type="password" class="form-control" id="exampleInputPassword1">
+					</div>
+
+					<div class="row d-flex justify-content-center">
+						<div class="col-12 d-flex justify-content-center">
+							<button type="submit" class="btn btn-primary btn-lg">
+								Connexion
+							</button>
+						</div>
+					</div>
+
+					<div class="row mt-3">
+						<div class="col-12 text-center">
+							<p>Vous n'avez pas de compte ?</p>
+							<a data-bs-toggle="modal" data-bs-target="#register-modal" class="text-info text-decoration-underline"> Inscrivez-vous</a>
+						</div>
+					</div>
+				</form>
+
+				<div class="row d-flex justify-content-center">
+					<img src="./assets/images/logo.png" alt="" style="width: 50%;">
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Modal d'inscription -->
+<div class="modal" id="register-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="modal-title fs-5" id="exampleModalLabel">Inscription</h1>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<div class="modal-body">
+
+				<p class="text-muted">Vous n'avez pas de compte ? Inscrivez-vous pour accéder à vos commandes, favoris et bien 
+					d'autres fonctionnalités. </p>
+
+				<form action="">
+					<div class="mb-3">
+						<label for="exampleInputEmail1" class="form-label">Prénom</label>
+						<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+					</div>
+					<div class="mb-3">
+						<label for="exampleInputEmail1" class="form-label">Nom</label>
+						<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+					</div>
+					<div class="mb-3">
+						<label for="exampleInputEmail1" class="form-label">Adresse mail</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+					</div>
+					<div class="mb-3">
+						<label for="exampleInputPassword1" class="form-label"> Mot de passe</label>
+						<input type="password" class="form-control" id="exampleInputPassword1">
+					</div>
+					<div class="mb-3">
+						<label for="exampleInputPassword1" class="form-label">Confirmez le mot de passe</label>
+						<input type="password" class="form-control" id="exampleInputPassword1">
+					</div>
+
+					<div class="row d-flex justify-content-center">
+						<small class="text-muted mb-2">Tous les champs sont obligatoires</small>
+						<div class="col-12 d-flex justify-content-center">
+							<button type="submit" class="btn btn-primary btn-lg">
+								Inscription
+							</button>
+						</div>
+					</div>
+
+					<div class="row mt-3">
+						<div class="col-12 text-center">
+							<p>Vous avez déjà un compte ?</p>
+							<a data-bs-toggle="modal" data-bs-target="#login-modal" class="text-info text-decoration-underline"> Connectez-vous</a>
+						</div>
+					</div>
+				</form>
+
+				<div class="row d-flex justify-content-center">
+					<img src="./assets/images/logo.png" alt="" style="width: 50%;">
+				</div>
+
+
+			</div>
+		</div>
+	</div>
 </div>
 
 
