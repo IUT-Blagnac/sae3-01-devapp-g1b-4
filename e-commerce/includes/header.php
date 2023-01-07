7d284a9a67315d8c500169097b5a6dcdf7c26f19
Andrew <andrew.p_pro@outlook.com>
2023-01-07 21:49:14 +0100

    Update header.php
    Misa a jour nombre icone panier

--- e-commerce/includes/header.php
+++ e-commerce/includes/header.php
@@ -13,7 +13,7 @@ session_start();
 	</div>
 	<div class="navigation">
 		<div class="search-bar">
-			<form method="get" action="recherche.php" name="formSearch">
+			<form method="get" action="search.php" name="formSearch">
 				<input type="text" name="rechercheUser" placeholder="Rechercher" class="search-input">
 				<button type="submit" class="search-button">
 					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
@@ -30,7 +30,44 @@ session_start();
 						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
 							<path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
 						</svg>
-						<span class="cart-badge">5</span>
+
+						<?php
+
+							require_once("./includes/connect.inc.php");
+
+							// Récupération du idPanier associé à l'utilisateur
+							$reqPanier = "SELECT DISTINCT P.idpanier FROM PANIER P, QUANTITEPANIER QP, PRODUIT PR
+                            WHERE P.idpanier = QP.idpanier AND P.idclient = :idClient AND PR.idproduit = QP.idproduit AND P.idpanier NOT IN
+                            (SELECT idpanier FROM COMMANDE C
+                            WHERE P.idpanier = C.idpanier)";
+
+							$idPanierUtilisateur = oci_parse($connect, $reqPanier);
+							oci_bind_by_name($idPanierUtilisateur, ":idClient", $_SESSION['idClientIdentifie']);
+							oci_execute($idPanierUtilisateur);
+
+							// Identifiant panier de l'utilisateur loggé
+							$idPanier = oci_fetch_assoc($idPanierUtilisateur);
+							
+							// Récupération de la quantité panier
+							$reqQuantite = "SELECT DISTINCT COUNT(IDPRODUIT) FROM QUANTITEPANIER WHERE IDPANIER = :idPanier";
+
+							$qttPanier = oci_parse($connect, $reqQuantite);
+							oci_bind_by_name($qttPanier, ":idPanier", $idPanier['IDPANIER']);
+							oci_execute($qttPanier);
+
+							// Quantité d'éléments dans le panier
+							$quantitePanier = oci_fetch_assoc($qttPanier);
+
+							$nbElementsPanier = $quantitePanier['COUNT(IDPRODUIT)'];
+
+						?>
+
+						<?php 
+							if ($nbElementsPanier != 0) {
+								echo '<span class="cart-badge">' . $nbElementsPanier . '</span>';
+							}
+						?>
+
 					</a>
 
 				</li>
@@ -43,14 +80,21 @@ session_start();
 								</svg>
 							</button>
 							<ul class="dropdown-menu">
-								<li><a class="dropdown-item" href="./formLogin.php"> Connexion</a></li>
-								<li><a class="dropdown-item text-primary fw-bold" href="./formRegister.php"> Inscription</a></li>
+								<?php
+								if (!isset($_SESSION['idClientIdentifie'])) {
+									echo '<li><a class="dropdown-item" href="./formLogin.php"> Connexion</a></li>';
+									echo '<li><a class="dropdown-item text-primary fw-bold" href="./formRegister.php"> Inscription</a></li>';
+								} else {
+									echo '<li><a class="dropdown-item" href="./profile.php"> Mon profil</a></li>';
+									echo '<li><a class="dropdown-item text-danger fw-bold" href="./deconnexion.php"> Déconnexion</a></li>';
+								}
+								?>
 							</ul>
 						</div>
 					</div>
 				</li>
 				<li>
-				<div>
+					<div>
 						<div class="dropdown">
 							<button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
 								<img src="https://cdn-icons-png.flaticon.com/512/206/206657.png" alt="" width="20">
