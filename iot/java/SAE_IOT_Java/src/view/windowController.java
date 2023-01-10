93ca29d74ecf2ce48faaa1fdb377f2efe37d0a1c
Aurélien <lefty.wolf.idk@hotmail.com>
2023-01-10 14:28:18 +0100

    Ajout de 9 diagrammes dans l'application

--- iot/java/SAE_IOT_Java/src/view/windowController.java
+++ iot/java/SAE_IOT_Java/src/view/windowController.java
@@ -5,47 +5,50 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
+import javafx.scene.layout.VBox;
 
 public class windowController implements Initializable {
-
+	
 	@FXML
-	private ListView<String> listViewData;
+	private VBox VboxGauche;
+	@FXML
+	private VBox VboxCentre;
+	@FXML
+	private VBox VboxDroite;
 	
 	@FXML
 	private RadioButton bActivity;
-	
 	@FXML
 	private RadioButton bCo2;
-	
 	@FXML
 	private RadioButton bHumidity;
-	
 	@FXML
 	private RadioButton bIllumination;
-	
 	@FXML
 	private RadioButton bInfrared;
-	
 	@FXML
 	private RadioButton bInfraredAndVisible;
-	
 	@FXML
 	private RadioButton bPressure;
-	
 	@FXML
 	private RadioButton bTemperature;
-	
 	@FXML
 	private RadioButton bTvoc;
 	
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
 		
 	}
 	
-	
+	public static void ajoutGraph(int graphId) {
+		 
+	}
+
 	
 }
